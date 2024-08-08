9b8ce908c2ed78cc534def912f8c7caff5234e33
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-08-08 08:27:01 +0200

    feat(master): fixin build

--- apps/back/src/index.ts
+++ apps/back/src/index.ts
@@ -3,13 +3,12 @@ import { version } from '../package.json'
 import { cors } from '@elysiajs/cors'
 import { worldModule } from './modules/world'
 import { logger } from "@bogeychan/elysia-logger"
-import { compression } from '@chneau/elysia-compression'
 
 
 const app = new Elysia()
   .use(cors())
   .use(logger())
-  .use(compression())
+  // .use(compression())
   .get('/', () => `api-version: ${version}`)
   .use(worldModule)
 
