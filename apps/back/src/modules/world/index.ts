a4170baddb30fa15f02667b6d2afb664345f1190
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-04 15:06:30 +0200

    feat(master): adding env

--- apps/back/src/modules/world/index.ts
+++ apps/back/src/modules/world/index.ts
@@ -14,7 +14,7 @@ export const worldModule = new Elysia({ prefix: '/worlds' })
   .use(
     cron({
       name: 'monthPass',
-      pattern: Patterns.everyHours(),
+      pattern: Bun.env.CRON_TIME ?? Patterns.everyHours(),
       async run() {
         const worldDbClient = new WorldsTable(db)
         const civilizationsDbClient = new CivilizationTable(db)
