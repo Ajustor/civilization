df5774984d7bbdd67b8239228043c4ed06e61b9f
Marc <mdelerue@gmail.com>
2024-09-09 15:35:00 +0200

    typo (#3)
    Co-authored-by: Marc Delerue <mdelerue@partner.auchan.fr>

--- apps/front/src/routes/rules/+page.svelte
+++ apps/front/src/routes/rules/+page.svelte
@@ -2,33 +2,32 @@
 	import { Block, List, ListItem } from 'konsta/svelte'
 </script>
 
-<Block class="m-auto flex lg:w-3/4 flex-col items-center justify-center">
+<Block class="m-auto flex flex-col items-center justify-center lg:w-3/4">
 	<h1 class="text-2xl">Les règles de la simulation</h1>
 	<List strong inset>
 		<ListItem title="Les règles du monde">
 			<ul slot="inner" class="list-disc">
 				<li>Toutes les heures un mois passe</li>
 				<li>
-					Tous les mois les citoyens doivent manger et avoir un toit sur leur tête afin de gagner de
-					la vie
+					Tous les mois les citoyens doivent manger et avoir un logement afin de gagner de la vie
 				</li>
-				<li>Un citoyen qui ateint l'age de 91 ans meurt</li>
+				<li>Un citoyen qui atteint l'age de 91 ans meurt</li>
 				<li>
-					Les citoyens peuvent se reproduire tous les mois si leur santé est au moins de 8 et qu'ils
-					ont entre 16 et 60 ans
+					Les citoyens peuvent se reproduire tous les mois si leur santé est d'au moins de 8 et
+					qu'ils ont entre 16 et 60 ans
 				</li>
 				<li>
-					Chaque citoyen a un métier dans la liste suivante
+					Chaque citoyen a un métier dans la liste suivante :
 					<ol class="list-inside list-decimal">
-						<li>Fermier (travaille de 4 a 70 ans et consome 1 de nourriture)</li>
-						<li>Charpentier (travaille de 12 a 60 ans et consome 2 de nourriture)</li>
+						<li>Fermier (travaille de 4 à 70 ans et consome 1 de nourriture)</li>
+						<li>Charpentier (travaille de 12 à 60 ans et consomme 2 de nourriture)</li>
 					</ol>
 				</li>
 				<li>
 					Il existe des bâtiments qui peuvent être construits et apportent des améliorations aux
 					métiers
 					<ol class="list-inside list-decimal">
-						<li>Maison (aide les citoyens a rester en vie)</li>
+						<li>Maison (aide les citoyens à rester en vie)</li>
 					</ol>
 				</li>
 				<li>
@@ -43,10 +42,13 @@
 				<li>
 					Le monde reconstitue ses réserves de ressources suivant ces règles
 					<ol class="list-inside list-decimal">
-						<li>au printemps<br /> 50 de nourriture avec un bonus de 20/civilisations active</li>
-						<li>en été<br /> 25 de nourriture avec un bonus de 10/civilisations active</li>
 						<li>
-							en automne<br /> 10 de nourriture avec un bonus de 5/civilisations active et 10 de bois
+							au printemps<br /> 50 de nourriture avec un bonus de 20 par civilisations active
+						</li>
+						<li>en été<br /> 25 de nourriture avec un bonus de 10 par civilisations active</li>
+						<li>
+							en automne<br /> 10 de nourriture avec un bonus de 5 par civilisations active et 10 de
+							bois
 						</li>
 						<li>en hiver<br />20 de bois</li>
 					</ol>
