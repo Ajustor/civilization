865218aa98901399dba013eadc574771af7e3995
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-17 14:16:17 +0200

    Master(master): try another thing

--- apps/back/src/libs/database/index.ts
+++ apps/back/src/libs/database/index.ts
@@ -1,7 +1,7 @@
 import { drizzle as tursoDrizzle } from 'drizzle-orm/libsql'
 import { createClient } from '@libsql/client'
 
-const url: string = Bun.env.LOCAL ? `file:civ-simulator.sqlite` : `${Bun.env.TURSO_URL}`
+const url: string = JSON.parse(Bun.env.LOCAL ?? 'true') ? `file:civ-simulator.sqlite` : `${Bun.env.TURSO_URL}`
 
 const sqliteClient = createClient({ url, authToken: Bun.env.TURSO_TOKEN })
 
