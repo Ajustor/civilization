36b0531c70cb9eec12ab4d4a41c4ed7c41c8b3a9
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-17 22:01:36 +0200

    Master(master): stop timer

--- apps/back/src/modules/world/index.ts
+++ apps/back/src/modules/world/index.ts
@@ -39,7 +39,7 @@ export const worldModule = new Elysia({ prefix: '/worlds' })
 
         try {
           await worldDbClient.saveAll(worlds)
-          console.log('A month has passed')
+          console.timeEnd('A month has passed')
         } catch (error) {
           console.error(error)
         }
