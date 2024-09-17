4e1ccfd0e4b6d9a63345bf27c504b56418abe698
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-17 17:10:09 +0200

    Master(master): try batches

--- apps/back/src/modules/civilizations/database.ts
+++ apps/back/src/modules/civilizations/database.ts
@@ -181,23 +181,26 @@ export class CivilizationTable {
   }
 
   async saveAll(civilizations: Civilization[]) {
+    const batch = []
     for (const civilization of civilizations) {
-      await this.client.update(civilizationTable).set({
+      batch.push(this.client.update(civilizationTable).set({
         livedMonths: civilization.livedMonths,
         people: civilization.people.map((person) => person.formatToEntity()),
         buildings: civilization.buildings.map((building) => building.formatToType()),
-      }).where(eq(civilizationTable.id, civilization.id))
+      }).where(eq(civilizationTable.id, civilization.id)))
       for (const civilizationResource of civilization.resources) {
-        await this.client.update(civilizationsResourcesTable).set({
+        batch.push(this.client.update(civilizationsResourcesTable).set({
           quantity: civilizationResource.quantity,
         }).where(
           and(
             eq(civilizationsResourcesTable.civilizationId, civilization.id),
             eq(civilizationsResourcesTable.resourceType, civilizationResource.type),
           )
-        )
+        ))
       }
     }
+
+    this.client.batch(batch)
   }
 
   async delete(userId: string, civilizationId: string) {
