d9fd4574b9d6c6c4a71dd89a9d145951907842fe
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-18 12:45:38 +0200

    feat(master): fix

--- apps/back/skipYears.ts
+++ apps/back/skipYears.ts
@@ -30,6 +30,7 @@ for (const world of worlds) {
   const worldCivilizations = await civilizationsDbClient.getAllByWorldId(world.id)
   world.addCivilization(...worldCivilizations.filter((civilization) => civilization.people.length).sort(() => Math.random() - 0.5))
   for (let i = 0; i < +values.years * 12; i++) {
+    console.log(`Passing a month ${i}/${+values.years * 12}`)
     world.passAMonth()
   }
   await civilizationsDbClient.saveAll(worldCivilizations)
