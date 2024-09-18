ad7f308765e48c1a6ef5037d8fd28de4a6c934a2
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-09-18 17:36:52 +0200

    feat(master):

--- apps/front/src/services/api/client.ts
+++ apps/front/src/services/api/client.ts
@@ -1,6 +1,6 @@
 import { treaty } from '@elysiajs/eden'
 import type { App } from '@ajustor/civ-api'
-import { BACK_URL } from '$env/static/public'
+import { BACK_URL } from '$env/static/private'
 
 export const client = treaty<App>(BACK_URL, {
   fetch: {
