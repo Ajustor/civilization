3bf489eca1fbc9f50c576bb8929683f08bdc639d
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-04 16:36:56 +0200

    Master(master): reuse split

--- apps/front/src/routes/my-civilizations/[slug]/+page.ts
+++ apps/front/src/routes/my-civilizations/[slug]/+page.ts
@@ -8,9 +8,11 @@ export const load: PageLoad = async ({ parent, params }) => {
   const data = await parent()
 
   const civilization = await getMyCivilizationFromId(data.authToken ?? '', params.slug).catch((error) => console.error('An error occured', error))
+
   if (!civilization) {
     redirect(302, '/')
   }
+
   return {
     civilization,
   }
