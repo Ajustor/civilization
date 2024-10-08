32722391469b4293d8cf42211171d893996c24d3
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-08 17:44:38 +0200

    Master(master): remove useless favicons

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -38,20 +38,9 @@ export default defineConfig({
 				background_color: "#ffffff",
 				icons: [
 					{
-						src: '/pwa-192x192.png',
-						sizes: '192x192',
-						type: 'image/png',
-					},
-					{
-						src: '/pwa-512x512.png',
+						src: '/favicon.png',
 						sizes: '512x512',
 						type: 'image/png',
-					},
-					{
-						src: '/pwa-512x512.png',
-						sizes: '512x512',
-						type: 'image/png',
-						purpose: 'any maskable',
 					},
 				],
 			},
