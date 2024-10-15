645744bd5623c63aadff64d25d245a6b9f36ee4c
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-15 09:21:57 +0200

    Master(master): try to reduce size

--- apps/back/src/modules/people/service.ts
+++ apps/back/src/modules/people/service.ts
@@ -6,12 +6,21 @@ export class PeopleService {
   constructor(private readonly civilizationService: CivilizationService) {
 
   }
-  public async getPeopleFromCivilization(civilizationId: string): Promise<PeopleType[]> {
+  public async getPeopleFromCivilization(civilizationId: string, withLineage = false): Promise<PeopleType[]> {
     const civilization = await this.civilizationService.getById(civilizationId)
     if (!civilization) {
       throw new Error(`No civilization found for ${civilizationId}`)
     }
 
-    return civilization.people.map((person) => person.formatToType())
+    return civilization.people.map((person) => {
+      const formatedPerson = person.formatToType()
+
+      if (!withLineage) {
+        delete formatedPerson.lineage
+      }
+
+      return formatedPerson
+    })
   }
+
 }
