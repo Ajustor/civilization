7c2b0b7e7f01d04dce819fc45b124062118fee9c
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-16 19:14:07 +0200

    Feat(adding-upgraded-work): start adding new types for buildings

--- packages/simulation/src/buildings/kiln.ts
+++ packages/simulation/src/buildings/kiln.ts
@@ -1,9 +1,21 @@
 import { ResourceTypes } from '../resource'
-import { Building, BuildingType, ConstructionCost } from '../types/building'
+import { BuildingType, ConstructionCost, ProductionBuilding } from '../types/building'
 import { BuildingTypes } from './enum'
 
-export class Kiln implements Building {
+export class Kiln implements ProductionBuilding {
   constructor(public readonly capacity: number, public readonly count: number) { }
+
+  inputResources = [
+    {
+      resource: ResourceTypes.WOOD,
+      amount: 5
+    }
+  ]
+
+  outputResources = [{
+    resource: ResourceTypes.CHARCOAL,
+    amount: 10
+  }]
 
   public static constructionCosts: ConstructionCost[] = [{
     resource: ResourceTypes.STONE,
@@ -21,7 +33,7 @@ export class Kiln implements Building {
     return {
       capacity: this.capacity,
       type: this.getType(),
-      count: this.count
+      count: this.count,
     }
   }
 }
--- packages/simulation/src/types/building.ts
+++ packages/simulation/src/types/building.ts
@@ -1,5 +1,8 @@
 import type { BuildingTypes } from '../buildings/enum'
+import { OccupationTypes } from '../people/work/enum'
 import { ResourceTypes } from '../resource'
+
+export type ProduceResource = { resource: ResourceTypes, amount: number }
 
 export type BuildingType = {
   id?: string
@@ -19,4 +22,14 @@ export interface Building {
 
   getType(): BuildingTypes
   formatToType(): BuildingType
+}
+
+export interface ProductionBuilding extends Building {
+  inputResources: ProduceResource[]
+  outputResources: ProduceResource[]
+}
+
+export interface ExtractionBuilding extends Building {
+  workerType: OccupationTypes
+  outputResources: { resource: ResourceTypes, probability: number }[]
 }
