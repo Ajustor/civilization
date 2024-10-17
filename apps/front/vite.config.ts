939392fe4e9d44c902bdc4eedff49024c150cc77
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-17 14:10:25 +0200

    feat(feat):  "retrieve pwa"

--- apps/front/vite.config.ts
+++ apps/front/vite.config.ts
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig, type PluginOption } from 'vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 import { visualizer } from 'rollup-plugin-visualizer'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 
 const generateSW = process.env.GENERATE_SW === 'true'
 
@@ -22,43 +23,43 @@ export default defineConfig({
 		visualizer({ filename: 'stats.html', emitFile: true }) as PluginOption,
 		enhancedImages(),
 		sveltekit(),
-		// SvelteKitPWA({
-		// 	srcDir: './src',
-		// 	strategies: generateSW ? 'generateSW' : 'injectManifest',
-		// 	// you don't need to do this if you're using generateSW strategy in your app
-		// 	filename: generateSW ? undefined : 'prompt-sw.ts',
-		// 	manifest: {
-		// 		short_name: 'Civilisations',
-		// 		name: 'Civilisations',
-		// 		start_url: '/',
-		// 		scope: '/',
-		// 		display: 'standalone',
-		// 		theme_color: "#ffffff",
-		// 		background_color: "#ffffff",
-		// 		icons: [
-		// 			{
-		// 				src: '/favicon.png',
-		// 				sizes: '512x512',
-		// 				type: 'image/png',
-		// 			},
-		// 		],
-		// 	},
-		// 	injectManifest: {
-		// 		globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-		// 	},
-		// 	workbox: {
-		// 		globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-		// 	},
-		// 	devOptions: {
-		// 		enabled: true,
-		// 		suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
-		// 		type: 'module',
-		// 		navigateFallback: '/',
-		// 	},
-		// 	// if you have shared info in svelte config file put in a separate module and use it also here
-		// 	kit: {
-		// 		includeVersionFile: true,
-		// 	}
-		// }),
+		SvelteKitPWA({
+			srcDir: './src',
+			strategies: generateSW ? 'generateSW' : 'injectManifest',
+			// you don't need to do this if you're using generateSW strategy in your app
+			filename: generateSW ? undefined : 'prompt-sw.ts',
+			manifest: {
+				short_name: 'Civilisations',
+				name: 'Civilisations',
+				start_url: '/',
+				scope: '/',
+				display: 'standalone',
+				theme_color: "#ffffff",
+				background_color: "#ffffff",
+				icons: [
+					{
+						src: '/favicon.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+				],
+			},
+			injectManifest: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			workbox: {
+				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+			},
+			devOptions: {
+				enabled: true,
+				suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+				type: 'module',
+				navigateFallback: '/',
+			},
+			// if you have shared info in svelte config file put in a separate module and use it also here
+			kit: {
+				includeVersionFile: true,
+			}
+		}),
 	]
 })
