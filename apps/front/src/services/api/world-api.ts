385ff9bf3087f2c2fb1d00347834147b2e8dc42a
Alexandre Gambier (Ajustor) <alexandre.gambier78@gmail.com>
2024-10-17 14:44:23 +0200

    feat(feat):  "try something to update"

--- apps/front/src/services/api/world-api.ts
+++ apps/front/src/services/api/world-api.ts
@@ -24,6 +24,7 @@ export async function getWorldsStats() {
   const worldsStats = new Map<string, WorldStats>()
 
   for (const world of worlds) {
+    console.log('Getting world stats for world', { name: world.name, id: world.id })
     worldsStats.set(world.id, await getWorldStats(world.id, { withAliveCount: true, withDeadCount: true, withMenAndWomenRatio: true, withTopCivilizations: true }))
   }
   return worldsStats
