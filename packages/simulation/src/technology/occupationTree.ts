7a08c6edb4cb7d6840a9941c5eaf9ae14506f5b8
Marc Delerue <mdelerue@gmail.com>
2024-11-27 14:55:18 +0100

    cook & kitchen

--- packages/simulation/src/technology/occupationTree.ts
+++ packages/simulation/src/technology/occupationTree.ts
@@ -1,14 +1,19 @@
 import { OccupationTypes } from '../people/work/enum'
 
 export const OCCUPATION_TREE: { [key in OccupationTypes]?: OccupationTypes[] } =
-  {
-    [OccupationTypes.WOODCUTTER]: [
-      OccupationTypes.CARPENTER,
-      OccupationTypes.CHARCOAL_BURNER,
-    ],
-    [OccupationTypes.GATHERER]: [OccupationTypes.FARMER, OccupationTypes.MINER, OccupationTypes.KITCHEN_ASSISTANT],
-    [OccupationTypes.CHILD]: [
-      OccupationTypes.WOODCUTTER,
-      OccupationTypes.GATHERER,
-    ],
-  }
+{
+  [OccupationTypes.WOODCUTTER]: [
+    OccupationTypes.CARPENTER,
+    OccupationTypes.CHARCOAL_BURNER,
+  ],
+  [OccupationTypes.GATHERER]: [
+    OccupationTypes.FARMER,
+    OccupationTypes.MINER,
+    OccupationTypes.KITCHEN_ASSISTANT,
+  ],
+  [OccupationTypes.CHILD]: [
+    OccupationTypes.WOODCUTTER,
+    OccupationTypes.GATHERER,
+  ],
+  [OccupationTypes.KITCHEN_ASSISTANT]: [OccupationTypes.COOK],
+}
